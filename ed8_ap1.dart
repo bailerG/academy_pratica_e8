b371072e349220c21af0409d7cabcaa85bf2c132
Guilherme Bailer Motta <guilherme.b.motta@gmail.com>
2023-07-03 15:53:39 -0300

    AP1 Concluida

--- ed8_ap1.dart
+++ ed8_ap1.dart
@@ -1,1 +1,45 @@
-void main() {}
+import 'dart:math';
+
+void main() {
+  final aleatorio = Random().nextInt(5);
+
+  try {
+    qualGeneroFavorito(aleatorio);
+  } catch (e) {
+    print(e);
+  }
+}
+
+enum GenerosMusicais {
+  trap,
+  pagode,
+  samba,
+  rock,
+  pop,
+  rap,
+}
+
+void qualGeneroFavorito(int index) {
+  final generoFavorito = GenerosMusicais.values[index];
+
+  switch (generoFavorito) {
+    case GenerosMusicais.trap:
+      print("Meu genero favorito é trap");
+      break;
+    case GenerosMusicais.pagode:
+      print("Meu genero favorito é pagode");
+      break;
+    case GenerosMusicais.samba:
+      print("Meu genero favorito é samba");
+      break;
+    case GenerosMusicais.rock:
+      print("Meu genero favorito é rock");
+      break;
+    case GenerosMusicais.pop:
+      print("Meu genero favorito é pop");
+      break;
+    case GenerosMusicais.rap:
+      print("Meu genero favorito é rap");
+      break;
+  }
+}
