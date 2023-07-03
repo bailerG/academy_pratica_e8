b3a0eed80f3af318a0a779847589c0701e60c4ea
Guilherme Bailer Motta <guilherme.b.motta@gmail.com>
2023-07-03 16:04:22 -0300

    AP2 Concluida

--- ed8_ap2.dart
+++ ed8_ap2.dart
@@ -1,1 +1,23 @@
-void main() {}
+void main() {
+  Cachorro Toby = Cachorro();
+
+  Toby.comer();
+  Toby.beber();
+  Toby.latir();
+}
+
+abstract class Animal {
+  void comer() {
+    print("Comendo biscoito escondido: nhom nhom nhom nhom...");
+  }
+
+  void beber() {
+    print("Bebendo água do vaso sem ninguém ver: shlup shlup shlup...");
+  }
+}
+
+class Cachorro extends Animal {
+  void latir() {
+    print("Não fui eu!!!: AU AU");
+  }
+}
