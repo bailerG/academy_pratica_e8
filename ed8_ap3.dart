5c96762de4abd3cbd27b23c13bfb7c0457c49a53
Guilherme Bailer Motta <guilherme.b.motta@gmail.com>
2023-07-03 16:43:58 -0300

    AP3 Concluida

--- ed8_ap3.dart
+++ ed8_ap3.dart
@@ -1,1 +1,26 @@
-void main() {}
+void main() {
+  Sapo tobias = Sapo();
+
+  tobias.caminhar();
+  tobias.nadar();
+}
+
+abstract class AnimalTerrestre {
+  void caminhar() {}
+}
+
+abstract class AnimalAquatico {
+  void nadar() {}
+}
+
+class Sapo implements AnimalAquatico, AnimalTerrestre {
+  @override
+  void caminhar() {
+    print("Eu consigo caminhar");
+  }
+
+  @override
+  void nadar() {
+    print("Eu consigo nadar");
+  }
+}
