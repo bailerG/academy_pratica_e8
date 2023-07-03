c4d89e483024aeef2ab349b87b036dcacfa2dd17
Guilherme Bailer Motta <guilherme.b.motta@gmail.com>
2023-07-03 16:51:06 -0300

    AP4 Concluida

--- ed8_ap4.dart
+++ ed8_ap4.dart
@@ -1,1 +1,13 @@
-void main() {}
+import 'dart:math';
+
+void main() {
+  final aleatorio = Random().nextInt(999) + 1;
+
+  print("O dobro de $aleatorio é: ${Calculadora.dobro(aleatorio)}");
+}
+
+abstract class Calculadora {
+  static int dobro(int num) {
+    return num * 2;
+  }
+}
